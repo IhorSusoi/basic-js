02a6de9921a76d6b9dde4e4caf8fe2bcc44879d9
Ihor Susoi <ihor.susoi@gmail.com>
2023-04-14 17:29:10 +0300

    add part of dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let objectDNS={};
+  let arr=domains.slice();
+  for(let i=0; i<domains.length; i++){
+    arr[i]=arr[i].split('.');
+  }
+  return arr;
 }
 
 module.exports = {
